3a2a7cc05fcddb7b4c5cffe724b1d7b5ceb671db
root <valanjudu@gmail.com>
2024-01-18 00:27:53 +0530

    new files are added

--- regiterform.php
+++ regiterform.php
@@ -1,12 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>reg_form</title>
+</head>
+<link rel="stylesheet" href="/style.css">
+<body>
+
+<style>
 *{
     margin: 0%;
     padding: 0%;
     font-family: 'Poppins', sans-serif;
     box-sizing: border-box;
-    cursor: pointer;
+    
 }
 body{
-    height: 100vh;
+    height: 100%;
     width: 100%;
     display: flex;
     justify-content: center;
@@ -15,20 +26,20 @@ body{
 }
 form{
     width: 25rem;
-    height: 27rem;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     background: rgba(255, 255, 255,0.06);
-    box-shadow: 0 8px 32px 0 rgba(18, 18, 24, 0.37);
+    box-shadow: 0 8px 32px 0 black;
     border-radius: 30px;
     border-left: 1px solid rgba(255, 255, 255,.3);
     border-top: 1px solid rgba(255, 255, 255,.3);
 
 }
-h1{
-    font-size: 50px;
+h2{
+    margin-top: 10px;
     text-align: center;
-    color: beige;
+    color: aqua;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, .2);
     letter-spacing: 3px;
     margin-bottom: 5%;
@@ -55,7 +66,7 @@ input{
 button{
     width: 50%;
     margin: 3% auto;
-    color: white;
+    color: black;
     font-size: 15px;
     opacity: .7;
     background: rgba(255, 255, 255,.6);
@@ -67,6 +78,7 @@ button{
     box-shadow: 3px 3px 5px rgba(31, 38, 135, .37);
     border-left: 1px solid rgba(255, 255, 255,.6) ;
     border-top: 1px solid rgba(255, 255, 255,.6);
+    cursor: pointer;
 }
 a{
     font-size: 12px;
@@ -74,3 +86,29 @@ a{
     opacity: .7;
     
 }
+
+</style>
+
+
+<form action="" method="post">
+
+ <h2>REGISTER HERE</h2>
+ <label for="fullname">full name <img src="user.png" style="height: 1rem; width:1rem;"></label>
+ <input type="text" name="username" placeholder="Enter your full name" required>
+
+ <label for="gmail" >E-mail<img src="gmail.png" style="height: 1rem; width:1rem;"></label>
+<input type="text" name="email" placeholder="your email" required>
+
+<label for="phone-No">mobile<img src="phone.png" style="height: 1rem; width:1rem;"></label>
+<input type="tel" placeholder="mobile number" required>
+
+<label for="password">password <img src="password.png" style="height: 1rem; width:1rem;"></label>
+<input type="password" name="password" placeholder="strong password" required>
+
+<label for="password">confirm password <img src="password.png" style="height: 1rem; width:1rem;"></label>
+<input type="password" name="password" placeholder="strong password" required>
+
+<button type="submit" >register</button>
+</form>
+</body>
+</html>
